a56e36156199958a7b558c931010ee876d2d1532
dadoirie <damir.krupic@gmail.com>
2026-06-16 01:53:48 +0200

    removed tank furnace content since it's out of scope of assortedtweaksnfixes - is an own mod now

--- src/main/java/com/dadoirie/assortedtweaksnfixes/content/component/furnace_tank/FurnaceTankHost.java
+++
@@ -1,9 +0,0 @@
-package com.dadoirie.assortedtweaksnfixes.content.component.furnace_tank;
-
-import net.minecraft.world.item.ItemStack;
-
-public interface FurnaceTankHost {
-    ItemStack getItem(int slot);
-    void setItem(int slot, ItemStack stack);
-    void setChanged();
-}
